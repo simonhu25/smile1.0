deedc54311603b18db23deb7db497b1e6add785c
simonhu25 <40444736+simonhu25@users.noreply.github.com>
2018-06-20 20:02:09 -0700

    Fixed minor bugs + implemented SVM fitting.

--- face_recog.py
+++ face_recog.py
@@ -7,6 +7,8 @@ All rights reserved.
 
 from time import time
 import logging
+import warnings
+warnings.filterwarnings('ignore')
 
 import cv2
 import numpy as np
@@ -18,7 +20,7 @@ from sklearn import preprocessing as prep
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
 from keras.utils import to_categorical
@@ -35,8 +37,8 @@ logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
 Download the labels.
 These categorical features will be encoded using one-hot encoding.
 '''
-labels = np.load('truth_labels.npy')
-labels = to_categorical(labels,num_classes=2)
+labels = np.load('truth_labels.npy').ravel()
+#labels = to_categorical(labels,num_classes=2)
 
 '''
 Download the images.
@@ -44,11 +46,12 @@ These images will be uniformly resized to 100 x 100 (height, width) and stored i
 '''
 h = 100
 w = 100
+prod = h*w
 
 image_names = glob.glob('./images/*')
 n_images = len(image_names)
 
-images = np.array([np.array([cv2.resize(cv2.imread(file),(h,w),cv2.INTER_LANCZOS4) for file in image_names])])[0]
+images = np.array([np.array([cv2.resize(cv2.imread(file,0),(h,w),cv2.INTER_LANCZOS4) for file in image_names])])[0]
 
 '''
 Split the datasets (images and labels) into training and testing sets.
@@ -56,6 +59,14 @@ The following split will be used: 80% of the dataset will be used as training da
 '''
 random_seed = 2
 X_train,X_test,Y_train,Y_test = train_test_split(images,labels,test_size=0.2,random_state=random_seed)
+
+'''
+Flatten the images so that they are of the form n_images x (h * w)
+'''
+n_train = X_train.shape[0]
+n_test = X_test.shape[0]
+X_train = X_train.flatten().reshape(n_train,prod)
+X_test = X_test.flatten().reshape(n_test,prod)
 
 ###############################################################################
 
@@ -66,18 +77,18 @@ First iteration: 300 components
 '''
 n_components = 300
 
-print("Extracting the top %d eigenfaces from %d faces." % (n_components,X_train.shape[0]))
+print("\nExtracting the top %d eigenfaces from %d faces." % (n_components,X_train.shape[0]))
 t0 = time()
-pca = RandomizedPCA(n_components=n_components,whiten=True).fit(X_train)
-print("Extraction completed. Time elapsed: %0.4fs." % (time() - t0))
+pca = PCA(n_components=n_components,whiten=True,svd_solver='randomized',random_state=random_seed).fit(X_train)
+print("\nExtraction completed. Time elapsed: %0.4fs." % (time() - t0))
 
 eigenfaces = pca.components_.reshape((n_components,h,w))
 
-print("Projecting onto the eigenbasis.")
-t0 = time(90
+print("\n Projecting onto the eigenbasis.")
+t0 = time()
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
-print("Projection completed in: %0.4fs" % (time() - t0))
+print("\nProjection completed in: %0.4fs" % (time() - t0))
 
 ###############################################################################
 
@@ -85,9 +96,10 @@ print("Projection completed in: %0.4fs" % (time() - t0))
 Train a SVM to classify the images into "smile" and "not smile".
 Perform a Grid-Search to find the best hyper-parameters.
 '''
-param_grid = {'C':[1e3,5e3,1e5,5e4,1e5],'gamma':[0.0001,0.0005,0.001,0.005,0.01,0.1],'kernel'=['rbf','linear']}
+param_grid = {'C':[1e3,5e3,1e5,5e4,1e5],'gamma':[0.0001,0.0005,0.001,0.005,0.01,0.1],'kernel':['rbf','linear']}
 t0 = time()
-clf = GridSearchCV(SVC(kernel='rbf',class_weight='auto'),param_grid)
+print('\nTraining the SVM classifier.\n')
+clf = GridSearchCV(SVC(kernel='rbf',class_weight=None),param_grid,verbose=1)
 clf = clf.fit(X_train_pca,Y_train)
 print("Fitting done in: %0.4fs" % (time() - t0))
 print("The parameters are: ")
